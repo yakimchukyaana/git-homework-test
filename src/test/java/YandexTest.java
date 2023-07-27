d20044d558b1e7c0aa8f0f6ced9e05e7302b1cf2
yakimchukyaana <yakimchukyaana@gmail.com>
2023-07-27 17:54:04 +0500

    empty yandex test

---
+++ src/test/java/YandexTest.java
@@ -0,0 +1,12 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class YandexTest {
+    @Test
+    void yandexTest() {
+        
+    }
+}
